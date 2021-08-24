3f8fce0396119cbc2a18966f8ab24aa88895ba39
Lucas Fernando <lucas98fernando@gmail.com>
2021-08-24 14:21:38 -0300

    Criando models e inserindo dados no banco com o Sequelize

--- express-project/conection.js
+++ express-project/conection.js
@@ -21,3 +21,59 @@ sequelize.authenticate()
         console.log('Não foi possível realizar a conexão.')
         console.log(`Erro: ${erro}`)
     })
+
+// Model
+// Com o node.js é possível implementar um model que será responsável por criar tabelas, colunas e etc... Diretamente no banco
+// Criando um model para postagens, como o nome da tabela chamada 'postagens'
+const Postagem = sequelize.define('postagens', {
+    // Montando a tabela
+    // A diferença entre o tipo STRING e TEXT é a limitação de caracteres
+    titulo: {
+        // Definindo o tipo VARCHAR com o Sequelize para a columa titulo
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        // Definindo o tipo TEXT para a coluna conteudo
+        type: Sequelize.TEXT
+    }
+})
+
+// Criando registros na tabela postagens, esse comando é semelhante ao INSERT INTO
+// Postagem.create({
+//     título: 'Aprendendo node.js e mysql',
+//     conteudo: 'Aprendendo a realizar conexões ao MySQL com o Node.js, utilizando o Sequelize"'
+// })
+
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+
+// Criando registros na tabela usuarios, esse comando é semelhante ao INSERT INTO 
+Usuario.create({
+    nome: 'Lucas',
+    sobrenome: 'Fernando',
+    idade: 23,
+    email: 'lucas@gmail.com'
+})
+
+// Criando o model das postagens e de usuarios e sincronizando no MySQL
+// É recomendado excluir ou comentar os comandos abaixo, para evitar que as tabelas sejam criada novamente ao executar o nodemon
+
+// Postagem.sync({
+//     force: true
+// })
+
+// Usuario.sync({
+//     force: true
+// })
