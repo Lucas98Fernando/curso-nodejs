781a20315f2fbbe58bb62715daabe7a061c189ba
Lucas Fernando <lucas98fernando@gmail.com>
2021-08-25 13:58:00 -0300

    Iniciando os estudos do MongoDB e realizando conexão com o banco de dados utilizando o mongoose

---
+++ mongo/conection-mongo.js
@@ -0,0 +1,19 @@
+// O mongoose é um ODM utilizado para abstrair o MongoDB
+
+// Importando o mongoose
+const mongoose = require('mongoose')
+
+// Configurando o mongoose
+
+mongoose.Promise = global.Promise
+// Informando a URL que você deseja conectar o MongoDB, nesse exemplo será criada uma conexão local com o nome do banco aprendendomongo
+mongoose.connect('mongodb://localhost/aprendendomongo', {
+        // Para evitar alguns erros de conexão
+        useMongoClient: true
+    })
+    .then(() => {
+        console.log('Conexão com o MongoDB estabelecida com sucesso!')
+    })
+    .catch((erro) => {
+        console.error(`Não foi possível realizar a conexão com o MongoDB: ${erro}`)
+    })
